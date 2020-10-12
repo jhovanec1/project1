bc70d7261bb1fea54fb6fd9c38342ff6fc94216c
jhovanec1 <hovanecjoseph@johndeere.com>
2020-10-12 10:30:54 -0500

    created function to animate array

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const blue = document.querySelector('#downright');
 const startButton = document.querySelector('#start');
 const playerButton = document.querySelector('#play1');
 startButton.addEventListener('click', startTimer);
-playerButton.addEventListener('click',stopTimerStart);
+// playerButton.addEventListener('click',stopTimerStart);
 
 
 var boardLights = [red, green, yellow, blue];
@@ -17,51 +17,37 @@ let countTop = 0;
 let level = 10; 
 let levelArray = [];
 
-// Start Game color sequence, also practice with how to animate
-
-function startGame(){
-    // while(countTop<300){
-        if(count<1){
-            red.style.opacity = "1.0";
-            yellow.style.opacity = ".2";
-            count ++;
-        }else if(count>=1 && count<2) {
-            green.style.opacity = "1.0";
-            red.style.opacity = ".2";
-            count ++;
-        }else if(count>=2 && count<3) {
-            blue.style.opacity = "1.0";
-            green.style.opacity = ".2";
-            count ++;
-        }else if(count>=3 && count<4) {
-            blue.style.opacity = ".2";
-            yellow.style.opacity = "1.0";
-            count = 0;
-        }    
-    
-    console.log(count);
-    countTop ++;
-    console.log(countTop);
-    // }
-    
-}
-
 // Starts the start up sequence upon clicking START button in window
 
 function startTimer(){
-    let startTime = setInterval(startGame,100);
-}
-
-// Attempt to stop the startup sequence
-
-function stopTimerStart(){
-    // if(countTop >= 40){
-    //     clearInterval(startTime);
-    //     startButton.removeEventListener('click', startTimer)
-    //     countTop = 0;
-    // }else{
-    //     console.log("doing nothing");
-    // }
+    let startTime = setInterval(function(){
+        if(countTop === 30 || countTop === 31){
+            clearInterval(startTime);
+            red.style.opacity = ".2";
+            yellow.style.opacity = ".2";
+            blue.style.opacity = ".2";
+            green.style.opacity = ".2";
+        }
+            if(count<1){
+                red.style.opacity = "1.0";
+                yellow.style.opacity = ".2";
+                count ++;
+            }else if(count>=1 && count<2) {
+                green.style.opacity = "1.0";
+                red.style.opacity = ".2";
+                count ++;
+            }else if(count>=2 && count<3) {
+                blue.style.opacity = "1.0";
+                green.style.opacity = ".2";
+                count ++;
+            }else if(count>=3 && count<4) {
+                blue.style.opacity = ".2";
+                yellow.style.opacity = "1.0";
+                count = 0;
+            } 
+            countTop ++;
+            console.log(countTop);   
+    },100);
 }
 
 //Creates array which will be converted to colors and then ran through visual function. 
@@ -74,42 +60,49 @@ function randomizer(){
     }
 }
 randomizer();
-// console.log(levelArray);
-
 
 
 function gamePlay(levelArray) {
-    for(let i =0;i<levelArray.length;i++){
-        let j = levelArray[i];
-        setTimeout(gameLogic(j), 100);
-        function gameLogic(j) {
+        
+        levelArray.forEach(function(j, index){setTimeout(function(){
             if(j===1){
                 red.style.opacity = "1.0";
                 yellow.style.opacity = ".2";
                 green.style.opacity = ".2";
                 blue.style.opacity = ".2";
                 console.log('red')
+                i ++;
             }else if(j===2){
                 red.style.opacity = ".2";
                 yellow.style.opacity = ".2";
                 green.style.opacity = "1.0";
                 blue.style.opacity = ".2";
                 console.log('green');
+                i ++;
             }else if(j===3){
                 red.style.opacity = ".2";
                 yellow.style.opacity = ".2";
                 green.style.opacity = ".2";
                 blue.style.opacity = "1.0";
                 console.log('blue');
+                i ++;
             }else if(j===4){
                 red.style.opacity = ".2";
                 yellow.style.opacity = "1.0";
                 green.style.opacity = ".2";
                 blue.style.opacity = ".2";
                 console.log('yellow');
+                i ++;
             }
-        }
-        console.log(j);
+        
+        }, 500* index);
+    });
     }
-}
+
 gamePlay(levelArray);
+
+// var intervalId = setInterval(function(){
+//     var timoutId = setTimeout(function(){ 
+//         console.log("wait for me!");
+//     }, 1000);
+//  }, 1000);
